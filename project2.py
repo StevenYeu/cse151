ead97ecdb57563e2825bed4900877555c80b5c78
Shiyao Liu <Shl166@ucsd.edu>
2016-04-17 16:45:53 -0700

    proj2 done

--- project2.py
+++ project2.py
@@ -1,7 +1,7 @@
 import numpy, math, csv, heapq
 import scipy.spatial.distance as eu
 import numpy.matlib as mat
-from statistics import mode
+from statistics import *
 #Shiyao Liu, A10626758
 
 class sample:
@@ -48,7 +48,21 @@ class sample:
         return mean, std
 
     def load(self):
-        self.matrix = numpy.array(list(csv.reader(open(self.filename,"r"),delimiter=','))).astype('float')
+        if self.filename == 'abalone.data':
+            gender = []
+            with open(self.filename, 'r') as f:
+                for line in f:
+                    gender.append(line[0])
+
+            male = [1 if i == 'M' else 0 for i in gender]
+            female = [1 if i == 'F' else 0 for i in gender]
+            infant = [1 if i =='I' else 0 for i in gender]
+            self.matrix = numpy.array(list( numpy.loadtxt(self.filename, delimiter=",",usecols=(1,2,3,4,5,6,7,8))))
+            self.matrix = numpy.insert(self.matrix,0,male,axis=1)
+            self.matrix = numpy.insert(self.matrix,0,female,axis=1)
+            self.matrix = numpy.insert(self.matrix,0,infant,axis=1)
+        else:
+            self.matrix = numpy.array(list(csv.reader(open(self.filename,"r"),delimiter=','))).astype('float')
 
     def split(self):
 
@@ -59,10 +73,7 @@ class sample:
         _, training_size = numpy.shape(self.training_set)
         self.training_set = numpy.array(self.training_set)
         self.training_label = self.training_set[:, training_size-1]
-        print(self.training_label[0])
         self.training_set = self.training_set[:, 0:training_size-1]
-        print(self.training_set[0])
-
         self.test_set = [self.matrix[i] for i in test_index]
         _, test_size = numpy.shape(self.test_set)
         self.test_set = numpy.array(self.test_set)
@@ -96,28 +107,34 @@ class sample:
         for ind, i in enumerate(self.test_set):
             temp = [(eu.euclidean(i, data), self.training_label[index]) for index, data in enumerate(self.training_set)]
             k_smallest = [item[1] for item in heapq.nsmallest(k, temp)]
-            majority_label = mode(k_smallest)
-            #print(majority_label)
+            try:
+                majority_label = mode(k_smallest)
+            except StatisticsError:
+                majority_label = max(set(k_smallest), key=k_smallest.count)
+
             if self.test_label[ind] != majority_label:
                 error += 1
             prediction.append(majority_label)
         error_rate = error/float(len(self.test_set))*100.0
         return error_rate, prediction, self.test_label
 
+
 def confusionMatrix(actual,pred):
-    con = [[0,0],[0,0]]
+    con = numpy.zeros((max(actual)+1,max(actual)+1))
+
     z = zip(actual,pred)
     for (a,b) in z:
         con[int(a)][int(b)] += 1
+
+    output = open('output.txt', "w")
+    for item in con:
+        output.write("%s\n" % item)
     return con
 
-
-
-
 if __name__ == '__main__':
-    sample = sample('Seperable.csv')
+    sample = sample('abalone.data')
     sample.setCounter()
-    print(sample.sampler(1))
+    sample.sampler(1)
     sample.load()
     sample.split()
     sample.set_training(sample.zscale(sample.training_set))
@@ -126,12 +143,11 @@ if __name__ == '__main__':
     for i in range(1, 10, 2):
         rate, prediction, result = sample.kNN(i)
         res_dict[rate] = (prediction, result)
-        print(i, rate)
+    print(9, rate)
+
     prediction, result = res_dict[min(res_dict)]
     print(confusionMatrix(result, prediction))
-    #for i in range(1,6):
-    #    sample.setCounter()
-    #    print(sample.sampler(10 ** i))
+
 
 
 
