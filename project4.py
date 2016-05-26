1d6608ef7ff97b599ed3eea29a7f0d66ca33d624
stevenyeu <yeusteven@gmail.com>
2016-05-25 18:23:24 -0700

    Done , Refactor code

--- project4.py
+++ project4.py
@@ -1,16 +1,141 @@
 __author__ = 'Shawn'
 
-import sys, csv, Functions, copy
+import sys, csv, copy
 import numpy as np
-import functools as fun
 import numpy.matlib as mat
 import scipy.spatial.distance as eu
-import Functions
-from sklearn.metrics import mean_squared_error
+
+import matplotlib.pyplot as plt
+
+class Cluster:
+
+    def __init__(self,center):
+        self.center = center
+        self.points = []
+        self.labels = []
+        self.WCSS = 0
+
+    def setCenter(self,center):
+        self.center = center
+
+    def addPoints(self,point,label):
+        self.points.append(point)
+        self.labels.append(label)
+
+    def resetPoints(self):
+        self.points = []
+        self.labels = []
+
+    def updateCenter(self):
+        self.center = np.array(self.points).mean(0)
+
+    def getCenter(self):
+        return  self.center
+
+    def getPoints(self):
+        return self.points
+
+    def getLabels(self):
+        return self.labels
+
+    def computeWCSS(self):
+        sum_list =[]
+        for i in self.points:
+            dist = eu.euclidean(i,self.center)
+            sum_list.append(dist**2)
+        self.WCSS = sum(sum_list)
+        return self.WCSS
+
+
+
+def Kmeans(k,data,labels):
+    clusters = {}
+
+    # Create Cluster and Centrids
+    for i in range(k):
+        clusters[i] = Cluster(data[i])
+
+    # Assign Points to Clusters
+    clusterData(k,data,labels,clusters)
+
+    changed = False
+
+    oldcenters = getAllCenters(clusters)
+
+    while True:
+        updateCenters(clusters)
+        clusterDataAlt(data,labels,clusters)
+        centers = getAllCenters(clusters)
+        if hasChanged(oldcenters,centers):
+            #changed = True
+            oldcenters = copy.deepcopy(centers)
+        else:
+            break
+
+    WCSS = []
+
+    for key,value in clusters.items():
+        WCSS.append(value.computeWCSS())
+
+    means, stds, = getMeanandSTD(clusters)
+
+    return WCSS, clusters, means, stds
+
+
+def getMeanandSTD(clusters):
+    means = []
+    stds = []
+    for k,v in clusters.items():
+        mean = np.mean(v.getPoints(),axis=0)
+        std = np.std(v.getPoints(),axis=0)
+        means.append(mean)
+        stds.append(std)
+    return means,stds
+
+
+def hasChanged(old,new):
+    oldmean = [np.mean(i) for i in old]
+    newmean = [np.mean(i) for i in new]
+    cmp = [i for (i, j) in zip(oldmean, newmean) if i != j]
+    if cmp:
+        return True
+    else:
+        return False
+
+
+def clusterData(k,data,labels,clusters):
+    for p, l in zip(data[k:], labels[k:]):
+        assignCluster(clusters, p, l)
+
+
+def clusterDataAlt(data,labels,clusters):
+    for p, l in zip(data, labels):
+        assignCluster(clusters, p, l)
+
+
+def assignCluster(clusters,point,label):
+    dist_list = []
+    for key, values in clusters.items():
+        dist = eu.euclidean(values.getCenter(),point)
+        dist_list.append((dist,key))
+    closest = min(dist_list)
+    clusters[closest[1]].addPoints(point,label)
+    return clusters[closest[1]]
+
+
+def updateCenters(clusters):
+    for key,value in clusters.items():
+        value.updateCenter()
+        value.resetPoints()
+
+def  getAllCenters(clusters):
+    centers = [value.getCenter() for key, value in clusters.items()]
+    return centers
 
 
 def append_ones(m):
     return np.concatenate((m, np.ones((m.shape[0], 1))), axis=1)
+
 
 
 def zscale(setname):
@@ -25,71 +150,6 @@ def zscale(setname):
     # z scale
     newmat = (setname - mean_matrix)/std_matrix
     return newmat
-
-def cluster(k, data, labels):
-    cluster_list = [[] for i in range(int(k))]
-    center_list = [data[i] for i in range(int(k))]
-    cluster_list,cluster_label = cluster_set(k, data, center_list, cluster_list, labels)
-    Changed = False
-    while not Changed:
-        old_center_list = copy.deepcopy(center_list)
-        center_list = update_centers(cluster_list)
-        cluster_list, cluster_label = cluster_set(k, data, center_list, cluster_list,labels)
-        oldmean = [np.mean(i) for i in old_center_list]
-        newmean = [np.mean(i) for i in center_list]
-        cmp_l = [i for (i,j) in zip(oldmean,newmean) if i != j]
-        if cmp_l:
-            Changed = True
-        else:
-            break
-
-    sum_list = []
-
-    for n, i in list(enumerate(center_list)):
-        ed_list = []
-        for j in cluster_list[n]:
-            d = eu.euclidean(i, j)
-            ed_list.append(d**2)
-        sum = fun.reduce(lambda x, y: x + y, ed_list,0)
-        sum_list.append(sum)
-
-    mean = [np.array(i).mean(0) for i in cluster_list]
-    std = [np.array(i).std(0) for i in cluster_list]
-    print(np.array(cluster_list).shape[0])
-    return cluster_list, center_list, sum_list, mean, std, cluster_label
-
-def cluster_set(k, data, center_list, cluster_list,cluster_label):
-    new_labels = [[]  for i in range(len(cluster_list))]
-    new_cluster = [[]  for i in range(len(cluster_list))]
-    for n,j in list(enumerate(data[k:])):
-        d_list = []
-        b = n+k
-        for i in center_list:
-            d_list.append(eu.euclidean(i, j))
-        m = min(d_list)
-        ind = d_list.index(m)
-        new_cluster[ind].append(j)
-        new_labels[ind].append(cluster_label[b])
-    return new_cluster,new_labels
-
-
-
-def update_centers(cluster_list):
-    new_list = [[] for i in range(len(cluster_list))]
-    j = 0
-    for i in cluster_list:
-        centroid = np.array(i).mean(0)
-        new_list[j] = (centroid)
-        j = j+1
-    return new_list
-
-
-def getCluster(cluster_list, center_list, point):
-    dist = [eu.euclidean(i,point) for i in center_list]
-    closest = min(dist)
-    index = dist.index(closest)
-    return cluster_list[index]
-
 
 
 if __name__ == '__main__':
@@ -113,38 +173,64 @@ if __name__ == '__main__':
     X_test = data[train_num:, :-1]
     Y_test = data[train_num:, -1]
 
-    # X_means = np.mean(X_train, axis=0)
-    # X_stds = np.std(X_train, axis=0)
-    # X_train = (X_train - X_means) / X_stds
-    # X_test = (X_test - X_means) / X_stds
+    X_means = np.mean(X_train, axis=0)
+    X_stds = np.std(X_train, axis=0)
+    X_train = (X_train - X_means) / X_stds
+    X_test = (X_test - X_means) / X_stds
 
+    #
     X_train = append_ones(X_train)
     X_test = append_ones(X_test)
 
 
-    clusters, center, WCSS, mean, std, labels = cluster(int(sys.argv[2]), X_train, Y_train)
-    # print("Center")
-    # print(center)
-    # print("WCSS")
-    # print(sum(WCSS))
-    # print("Mean")
-    # print(mean)
-    # print("STD")
-    # print(std)
+    WCSSs= []
+    RMSEs = []
+    ks = [1,2,4,8,16]
+    for k in ks:
+        WCSS, clusters, means, stds = Kmeans(k, X_train, Y_train)
+        WCSSs.append(sum(WCSS))
+        np.savetxt("Center, k = " + str(k) + ".csv", getAllCenters(clusters), delimiter=',')
 
-    beta_list = []
-    for c, l in zip(clusters,labels):
-        beta, _, _, _ = np.linalg.lstsq(c, l)
-        beta_list.append((c,beta))
+        np.savetxt("Means, k = " + str(k) + ".csv", means, delimiter=',')
 
-    RMSE = 0
-    for x, y in zip(X_test, Y_test):
-        model = getCluster(clusters, center, x)
-        for a, b in beta_list:
-            if a == model:
-                beta = b
-        RMSE += (y - np.dot(beta, x)) ** 2
+        np.savetxt("STDs, k = " + str(k) + ".csv", stds, delimiter=',')
 
-    print(np.sqrt((RMSE)/len(X_test)))
+        RMSE = 0
 
-    #beta,_,_,_  = np.linalg.lstsq(X_train,Y_train)
+
+        for x, y in zip(X_test,Y_test):
+            cluster = assignCluster(clusters,x,y)
+            new_x = x[:-1]
+            points = cluster.getPoints()
+            points = np.delete(points,-1,1)
+            labels = cluster.getLabels()
+            unscaleMean = X_means[:len(points)]
+            unscaleSTD = X_stds[:len(points)]
+            new_x = (new_x * unscaleSTD) + unscaleMean
+            points = (points * unscaleSTD) + unscaleMean
+            #np.where(~points.any(axis=0))[0]
+            beta, _, _, _ = np.linalg.lstsq(points, labels)
+            RMSE += (y - np.dot(beta, new_x)) ** 2
+
+        RMSEs.append(np.sqrt((RMSE) / len(X_test)))
+
+    np.savetxt("RSME.csv", RMSEs, delimiter=',')
+    np.savetxt("WCSS.cvs", WCSSs,delimiter=',')
+
+
+    # Plot Graph
+
+    # fig = plt.figure()
+    # plt.plot(ks,WCSSs)
+    # fig.suptitle("WCSS vs K", fontsize=20)
+    # plt.xlabel('K', fontsize=18)
+    # plt.ylabel('WCSS', fontsize=16)
+    # plt.show()
+
+    # Plot Graph
+    fig = plt.figure()
+    plt.plot(ks, RMSEs)
+    fig.suptitle("RSME vs K", fontsize=20)
+    plt.xlabel('K', fontsize=18)
+    plt.ylabel('RSME', fontsize=16)
+    plt.show()
